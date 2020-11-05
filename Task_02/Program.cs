9d439d4196e409da68a8fc319018a003bba12233
Alexey Zobnin <ayuzobnin@edu.hse.ru>
2020-11-05 14:14:41 +0300

    Solved 1.2.2

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -25,19 +25,38 @@ namespace Task_02 {
 		static void Main(string[] args) {
 			int p;
 			// TODO : Реализовать ввод целочисленного значения.
-			p =;
+			p = int.Parse(Console.ReadLine());
 
 			// TODO : Посчиать и вывести результат.
+			Console.WriteLine(MaxPermutation(p));
 		}
+		
+		static void SwapDigits(ref int a, ref int b)
+        {
+			a ^= b;
+			b ^= a;
+			a ^= b;
+        }
 
 		static int MaxPermutation(int x) {
 			// TODO : Получить цифры числа используя арифметические операции.
-			int firstDigit = ,
-				secondDigit = ,
-				thirdDigit = ;
-
+			int firstDigit = x/100,
+				secondDigit = x/10%10,
+				thirdDigit = x%10;
+			if (thirdDigit > secondDigit)
+            {
+				SwapDigits(ref secondDigit, ref thirdDigit);
+            }
+			if (thirdDigit > firstDigit)
+            {
+				SwapDigits(ref firstDigit, ref thirdDigit);
+            }
+			if (secondDigit > firstDigit)
+			{
+				SwapDigits(ref firstDigit, ref secondDigit);
+			}
 			// TODO : Собрать новое число и вернуть его. 
-			int max =
+			int max = firstDigit * 100 + secondDigit * 10 + thirdDigit;
 			return max;
 		}
 	}
